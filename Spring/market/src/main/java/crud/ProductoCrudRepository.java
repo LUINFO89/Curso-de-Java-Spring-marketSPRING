7aa074f77f97f04543dd5cee8f8a00af4f63284e
Luis Hernando Soto Mesa <59453252+LUINFO89@users.noreply.github.com>
2023-05-24 06:54:37 -0500

    crud

--- Spring/market/src/main/java/crud/ProductoCrudRepository.java
+++ Spring/market/src/main/java/crud/ProductoCrudRepository.java
@@ -1,4 +1,15 @@
 package crud;
 
-public class ProductoCrudRepository {
+import market.entity.Producto;
+import org.springframework.data.repository.CrudRepository;
+import java.util.Optional;
+
+import java.util.List;
+
+public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+
+ //@Query (Value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true);
+ List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+ Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 }
+
